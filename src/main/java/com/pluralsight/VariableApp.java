72ec1d9686882a8f538f08c4e575af7689093f41
chrisbunny <christopherjbunny@gmail.com>
2026-04-08 14:35:45 -0500

    Finished step #1

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+
+        //Step #1
+        String favoriteColor = "Green";
+        int yearStarter = 2026;
+        char middleInitial = 'J';
+        boolean pets = false;
+        String niceMessage = "I hope you have a great day!";
+
+        //Step #2
+
+    }
+}
