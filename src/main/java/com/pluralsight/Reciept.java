7a041067814392b31a2476ead9fe033041960a76
chrisbunny <christopherjbunny@gmail.com>
2026-04-08 14:51:44 -0500

    Finished Step #3

---
+++ src/main/java/com/pluralsight/Reciept.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class Reciept {
+    public static void main(String[] args) {
+
+        //Step #3
+        String itemName = "apples";
+        double itemPrice = 7.10;
+        int quantityVariable = 3;
+
+        System.out.println("You bought " + quantityVariable + " " + itemName + " for $" + itemPrice);
+    }
+}
